968d189170280e99ceb59ffbe0a958ac99a272f5
catur <cds.obc@gmail.com>
2023-02-12 22:07:26 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
   });
  
  
